7d0b7ebdaae0990a91e6e86ac04480934e322fbb
wangwei <qingu@aliyun.com>
2019-04-23 16:02:58 +0800

    use Cache Facade

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 //use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 //use Illuminate\Support\Arr;
-//use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Cache;
 
 class NoteController extends Controller
 {
